0cc2e45076816b3ac5be78130c10400511965450
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-07-06 12:04:36 +0200

    Added new method PasswordStrengthAnalyzer::_simplifyPassword() and new coeficient simplicity_factor

--- src/password_strength_analyzer.php
+++ src/password_strength_analyzer.php
@@ -4,16 +4,84 @@ namespace Yarri;
 class PasswordStrengthAnalyzer {
 
 	protected $coefficients = [];
-	
-	function analyze($password){
-		$password = new \String4($password);
-		$chars = $password->chars(["stringify" => false]);
 
-		// Treatment for older versions of String4
-		foreach($chars as $k => $char){
-			if(is_object($char)){ break; } // \String4
-			$chars[$k] = new \String4($char);
+	function analyze($password){
+		$password = (string)$password;
+
+		$score = $this->_analyze($password,$coefficients);
+
+		$password_simplified = $this->_simplifyPassword($password);
+		$score_simplified = $this->_analyze($password_simplified);
+		$simplicity_factor = 1;
+		if($score_simplified<$score){
+			$percent = ($score - $score_simplified) / ($score / 100.0);
+			$simplicity_factor = round(1 - ($percent/2)/100,4);
 		}
+
+		$score = $score * $simplicity_factor;
+
+		$coefficients["simplicity_factor"] = $simplicity_factor;
+
+		$this->coefficients = $coefficients;
+
+		return round($score);
+	}
+
+	function getCoefficients(){
+		return $this->coefficients;
+	}
+
+	/**
+	 *
+	 *	$this->_simplifyPassword("Heeello1#"); // "Helo1#"
+	 */
+	function _simplifyPassword($password){
+		while(1){
+			$orig_password = $password;
+			$password = preg_replace('/(.+)\1/','\1',$password);
+			if($orig_password === $password){
+				break;
+			}
+		}
+
+
+		// remove groups of 3 or more consecutive characters
+		$chars = $this->_chars($password);
+		$password = "";
+		$prev_char = null;
+		$skipped_chars = [];
+		foreach($chars as $char){
+			if(
+					$prev_char && 
+					(
+						($prev_char->match('/[a-z]/') && $char->match('/[a-z]/')) ||
+						($prev_char->match('/[A-Z]/') && $char->match('/[A-Z]/')) ||
+						($prev_char->match('/[1-9]/') && $char->match('/[1-9]/'))
+					) && (
+						(ord("$char") === ord("$prev_char") + 1) ||
+						(ord("$char") === ord("$prev_char") - 1)
+					)
+			){
+				if(!$skipped_chars){
+					$password .= $char;
+				}
+				$skipped_chars[] = $char;
+				$prev_char = $char;
+				continue;
+			}
+
+			$skipped_chars = [];
+
+			$prev_char = $char;
+
+			$password .= $char;
+		}
+
+		return $password;
+	}
+	
+	protected function _analyze($password,&$coefficients = []){
+		$chars = $this->_chars($password);
 
 		$unique_chars = [];
 		$unique_chars_by_type = [];
@@ -46,19 +114,30 @@ class PasswordStrengthAnalyzer {
 		$coefficients["types_used"] =					$this->_calcCoef(sizeof($types_used)-1,					1.5,		0.3,				0,			4);
 		$coefficients["type_transitions"] =		$this->_calcCoef(sizeof($type_transitions)-1,		1.3,		0.3,				0,			4);
 
-		$this->coefficients = $coefficients;
-
 		foreach($coefficients as $_type => $coefficient){
 			$score = $score * $coefficient;
 		}
 
-		$score = min(100,round($score));
+		$score = min(100.0,$score);
 
 		return $score;
 	}
 
-	function getCoefficients(){
-		return $this->coefficients;
+	/**
+	 *
+	 * @return String4[]
+	 */
+	protected function _chars($str){
+		$str = new \String4($str);
+		$chars = $str->chars(["stringify" => false]);
+
+		// Treatment for older versions of String4
+		foreach($chars as $k => $char){
+			if(is_object($char)){ break; } // \String4
+			$chars[$k] = new \String4($char);
+		}
+
+		return $chars;
 	}
 
 	protected function _calcCoef($x,$base,$multiplier,$offset = 0,$max = 2){
--- test/tc_password_strength_analyzer.php
+++ test/tc_password_strength_analyzer.php
@@ -5,8 +5,8 @@ class TcPasswordStrengthAnalyzer extends TcBase {
 		$analyzer = new Yarri\PasswordStrengthAnalyzer();
 
 		$score1 = $analyzer->analyze('thisipassword');
-		$score2 = $analyzer->analyze('thisISpasword');
-		$score3 = $analyzer->analyze('thisISpasword8!');
+		$score2 = $analyzer->analyze('thisISpassword');
+		$score3 = $analyzer->analyze('thisISpassword8!');
 
 		$this->assertTrue($score1>0);
 		$this->assertTrue($score2>0);
@@ -14,5 +14,40 @@ class TcPasswordStrengthAnalyzer extends TcBase {
 
 		$this->assertTrue($score2>$score1);
 		$this->assertTrue($score3>$score2);
+	}
+
+	function test_simplicity_factor(){
+		$analyzer = new Yarri\PasswordStrengthAnalyzer();
+
+		$score1 = $analyzer->analyze('hiWORLDhiWORLD');
+		$coeficients1 = $analyzer->getCoefficients();
+
+		$score2 = $analyzer->analyze('hiWORLDhiWZRLD');
+		$coeficients2 = $analyzer->getCoefficients();
+
+		$this->assertTrue($score1 < $score2);
+		$this->assertTrue(1.0 > $coeficients1["simplicity_factor"]);
+		$this->assertEquals(1.0,$coeficients2["simplicity_factor"]);
+	}
+
+	function test__simplifyPassword(){
+		$analyzer = new Yarri\PasswordStrengthAnalyzer();
+
+		$this->assertEquals("hiWorld",$analyzer->_simplifyPassword("hiWorld"));
+		$this->assertEquals("Helo#1",$analyzer->_simplifyPassword("Heeello#11"));
+		$this->assertEquals("",$analyzer->_simplifyPassword(""));
+
+		$this->assertEquals("ab",$analyzer->_simplifyPassword("ab"));
+		$this->assertEquals("ab",$analyzer->_simplifyPassword("abc"));
+		$this->assertEquals("xabx",$analyzer->_simplifyPassword("xabcdefx"));
+
+		$this->assertEquals("ba",$analyzer->_simplifyPassword("ba"));
+		$this->assertEquals("cb",$analyzer->_simplifyPassword("cba"));
+		$this->assertEquals("xfex",$analyzer->_simplifyPassword("xfedcbax"));
+
+		$this->assertEquals("hiWorld",$analyzer->_simplifyPassword("hiWorld"));
+		$this->assertEquals("Secret12",$analyzer->_simplifyPassword("Secret12"));
+		$this->assertEquals("Secret12",$analyzer->_simplifyPassword("Secret123"));
+		$this->assertEquals("Secret12",$analyzer->_simplifyPassword("Secret1234"));
 	}
 }
